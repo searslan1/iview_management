aacb0f308decae352f51282c9403bbb55d5e7d70
searslan1 <169062883+searslan1@users.noreply.github.com>
2024-11-18 17:45:02 +0300

    env

--- iView-web/vite.config.js
+++ iView-web/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5174,  // Burada portu 5174 olarak belirtiyorsunuz
+  build: {
+    outDir: 'dist', 
   },
 })
